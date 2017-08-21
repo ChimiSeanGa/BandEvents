c2580fda32527164b5453578bc81eb8a1aae67a0
Sean Gonzales <seang696@gmail.com>
2017-08-21 12:36:33 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,106 @@
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
+/**
+ * This skill supplies users with information from the Mustang Band calendar.
+ * This skill supports English. (en-US).
+ **/
+
+'use strict';
+
+const Alexa = require('alexa-sdk');
+const ical = require('ical');
+const http = require('http');
+const utils = require('util');
+
+var alexa;
+
+const APP_ID = undefined;
+
+var cal = 'https://calendar.google.com/calendar/ical/9aqoirufc0h1mrihk78us5mlgs%40group.calendar.google.com/public/basic.ics';
+
+
+function removeTags(str) {
+   if (str) {
+      return str.replace(/<(?:.|\n)*?>/gm, '');
+   }
+}
+
+function getUpcomingEvents(callback) {
+   var events = new Array();
+   var today = new Date();
+
+   ical.fromURL(cal, {}, function(err, data) {
+      for (var k in data) {
+         if (data.hasOwnProperty(k)) {
+            var ev = data[k];
+
+            var eventData = {
+               summary: removeTags(ev.summary),
+               location: removeTags(ev.location),
+               description: removeTags(ev.description),
+               start: ev.start
+            };
+
+            if (eventData.summary != undefined && eventData.start >= today)
+               events.push(eventData);
+         }
+      }
+
+      var nextEvent = getNextEvent(events);
+      callback(nextEvent);
+   });
+}
+
+function getNextEvent(eventList) {
+   var nextEvent = eventList[0];
+
+   for (var i = 0; i < eventList.length; i++) {
+      if (eventList[i].start < nextEvent.start)
+         nextEvent = eventList[i];
+   }
+
+   return nextEvent;
+}
+
+const handlers = {
+   'LaunchRequest': function () {
+      this.emit('BandEvents');
+   },
+   'BandEventsIntent': function () {
+      this.emit('BandEvents');
+   },
+   'BandEvents': function () {
+      getUpcomingEvents(function(output) {
+         var speechOutput = '';
+         speechOutput += 'Your next event is ';
+         speechOutput += output.summary;
+         speechOutput += ' on ';
+
+         var options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+         options.timeZone = 'UTC';
+
+         var date = output.start.toLocaleString('en-US', options);
+
+         speechOutput += date;
+         alexa.emit(':tellWithCard', speechOutput, 'Mustang Band', speechOutput);
+      });
+   },
+   'AMAZON.HelpIntent': function () {
+      const speechOutput = this.t('HELP_MESSAGE');
+      const reprompt = this.t('HELP_MESSAGE');
+      this.emit(':ask', speechOutput, reprompt);
+   },
+   'AMAZON.CancelIntent': function () {
+      this.emit(':tell', this.t('STOP_MESSAGE'));
+   },
+   'AMAZON.StopIntent': function () {
+      this.emit(':tell', this.t('STOP_MESSAGE'));
+   },
+};
+
+exports.handler = function (event, context) {
+   alexa = Alexa.handler(event, context);
+   alexa.APP_ID = APP_ID;
+   alexa.registerHandlers(handlers);
+   alexa.execute();
+};
